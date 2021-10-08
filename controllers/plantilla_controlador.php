e3aa53cf65ca37565a7f266efbc2b1f6adf7188c
KevinOsorio <kosorio200012@hotmail.com>
2021-10-08 18:17:49 -0500

    carteles

--- controllers/plantilla_controlador.php
+++ controllers/plantilla_controlador.php
@@ -29,11 +29,7 @@ class ControladorPlantilla{
     }
     public function ctrRegistro(){
         include "vistas/registro.php";
-    }
-    public function ctrCarteles(){
-        include "vistas/carteles.php";
-    }
-    public function ctrCargarLogin(){
+    }c function ctrCargarLogin(){
         include "../admin/templates/login.php";
     }
     
