cb8877459e15d38806e8c2f33e04b9ceaf700fd4
maro <madaroby99@gmail.com>
2021-10-08 19:39:13 -0500

    final

--- vistas/damian.php
+++ vistas/damian.php
@@ -328,30 +328,24 @@
 <!-- =            Footer            =
 =============================-->
 
-	<footer class="footer-main">
+<footer class="footer-main">
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
 					<div class="block text-center">
-						<div class="footer-logo">
-							<img style="width: 350px;" href="http://localhost/primercoloquio%20fin/principal.php"
-								src="assets/principal/images/logop.png" alt="logo" class="img-fluid">
+						<div class="footer-logo" style="display:flex;justify-content: space-evenly;">
+						<img style="width: 350px;" src="assets/principal/images/LogoUPTap.png" alt="">	
+						<img style="width: 460px;" href="http://localhost/primercoloquio%20fin/principal.php" src="assets/principal/images/logop.png" alt="logo" class="img-fluid">
 						</div>
 						<ul class="social-links-footer list-inline">
 							<li class="list-inline-item">
-								<a href="#"><i class="fa fa-facebook"></i></a>
+								<a href="https://www.facebook.com/UPTapachula"><i class="fa fa-facebook"></i></a>
 							</li>
 							<li class="list-inline-item">
-								<a href="#"><i class="fa fa-twitter"></i></a>
+								<a href="https://twitter.com/uptapachula?lang=es"><i class="fa fa-twitter"></i></a>
 							</li>
 							<li class="list-inline-item">
-								<a href="#"><i class="fa fa-instagram"></i></a>
-							</li>
-							<li class="list-inline-item">
-								<a href="#"><i class="fa fa-rss"></i></a>
-							</li>
-							<li class="list-inline-item">
-								<a href="#"><i class="fa fa-vimeo"></i></a>
+								<a href="https://www.instagram.com/uptapachula/"><i class="fa fa-instagram"></i></a>
 							</li>
 						</ul>
 					</div>
@@ -365,8 +359,8 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-md-6 align-self-center">
-					<div class="copyright-text">
-						<p><a href="#">Universidad Politecnica de Tapachula</a> &#169; 2021 Todos los derechos
+					<div class="copyright-text" style="width:610px">
+						<p><a href="#" style="color:#ffff">Universidad Politecnica de Tapachula</a> &#169; 2021 Todos los derechos
 							reservados.</p>
 					</div>
 				</div>
