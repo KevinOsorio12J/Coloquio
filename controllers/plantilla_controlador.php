0edddbed55cae50209a5d8b383dd5ac9d9fb4c74
maro <madaroby99@gmail.com>
2021-10-11 13:28:53 -0500

    Solucion eror

--- controllers/plantilla_controlador.php
+++ controllers/plantilla_controlador.php
@@ -3,34 +3,4 @@ class ControladorPlantilla{
     public function ctrIndex(){
         include "vistas/principal.php";
     }
-    public function ctrHilerio(){
-        include "vistas/hilerio.php";
-    }
-    public function ctrPedro(){
-        include "vistas/PedroTomas.php";
-    }
-    public function ctrEduardo(){
-        include "vistas/Eduardo.php";
-    }
-    public function ctrMancera(){
-        include "vistas/Mancera.php";
-    }
-    public function ctrOsbeli(){
-        include "vistas/Osbeli.php";
-    }
-    public function ctrCarlos(){
-        include "vistas/carlos.php";
-    }
-    public function ctrEfrain(){
-        include "vistas/efrain.php";
-    }
-    public function ctrDamian(){
-        include "vistas/damian.php";
-    }
-    public function ctrRegistro(){
-        include "vistas/registro.php";
-    } function ctrCargarLogin(){
-        include "../admin/templates/login.php";
-    }
-    
 }
